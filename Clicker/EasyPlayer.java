bef7160b9a2b494424ed8b419fefc15ea3f6d790
Marko <110738528+makiesushi@users.noreply.github.com>
2023-10-03 11:51:23 +0200

    Update EasyPlayer.java

--- Clicker/EasyPlayer.java
+++ Clicker/EasyPlayer.java
@@ -5,5 +5,18 @@ public class EasyPlayer extends Player {
     public EasyPlayer(long points, int cps, int priceCps, int priceCpsX, int pow, int pricePow, int pricePowX) {
         super(points, cps, priceCps, priceCpsX, pow, pricePow, pricePowX);
     }
+    
+    @Override
+    public void save(){
+        try{
+            PrintWriter pw = new PrintWriter("save.uwu");
+            pw.println(this.points + " " + this.cps + " " + this.priceCps + " " + this.priceCpsX + " " +
+                    this.pow + " " + this.pricePow + " " + this.pricePowX);
+            pw.flush();
+            pw.close();
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+    }
 
 }

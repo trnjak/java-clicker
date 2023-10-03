3251bb6251188e6f3dd4c5243ed544e6d4229975
Marko <110738528+makiesushi@users.noreply.github.com>
2023-10-03 11:52:17 +0200

    Update EasyPlayer.java

--- Clicker/EasyPlayer.java
+++ Clicker/EasyPlayer.java
@@ -5,11 +5,31 @@ public class EasyPlayer extends Player {
     public EasyPlayer(long points, int cps, int priceCps, int priceCpsX, int pow, int pricePow, int pricePowX) {
         super(points, cps, priceCps, priceCpsX, pow, pricePow, pricePowX);
     }
+
+    @Override
+    public void load(){
+        try{
+            FileInputStream fis = new FileInputStream("easy-save.uwu");
+            Scanner in = new Scanner(fis);
+            this.points = in.nextLong();
+            this.cps = in.nextInt();
+            this.priceCps = in.nextInt();
+            this.priceCpsX = in.nextInt();
+            this.pow = in.nextInt();
+            this.pricePow = in.nextInt();
+            this.pricePowX = in.nextInt();
+            Graphics.settingText(new Player(this.points, this.cps, this.priceCps, this.priceCpsX,
+                    this.pow, this.pricePow, this.pricePowX));
+            in.close();
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+    }
     
     @Override
     public void save(){
         try{
-            PrintWriter pw = new PrintWriter("save.uwu");
+            PrintWriter pw = new PrintWriter("easy-save.uwu");
             pw.println(this.points + " " + this.cps + " " + this.priceCps + " " + this.priceCpsX + " " +
                     this.pow + " " + this.pricePow + " " + this.pricePowX);
             pw.flush();
